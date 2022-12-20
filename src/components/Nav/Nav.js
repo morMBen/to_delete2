e69664c435368346758a7827391e30781755ff5c
Mor Mordechai Ben <mor6686@gmail.com>
2022-12-20 16:35:59 +0200

    test

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./style.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
 function Nav() {
   return (
-    <div className="nav">
-      <div className="nav__logo">App Logo</div>
-      <div className="nav__buttons"></div>
+    <div className='nav'>
+      <div className='nav__logo'>App Logo</div>
+      <div className='nav__buttons'></div>
     </div>
   );
 }
