08e62ac49c3b92c7127ab12d064f26c8f46f0593
EricChukoltyansky <kazohelmet@gmail.com>
2022-12-20 16:26:45 +0200

    deleted all navs

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './style.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 function Nav() {
   return (
-    <div className='nav'>
-      <div className='nav__logo'>App Logo</div>
-      <div className='nav__buttons'>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/signup'>Sign Up</Link>
-      </div>
+    <div className="nav">
+      <div className="nav__logo">App Logo</div>
+      <div className="nav__buttons"></div>
     </div>
   );
 }
