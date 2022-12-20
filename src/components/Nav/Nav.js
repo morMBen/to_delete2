d9fd8d1fcbc1e0574676c3247802c8001005c087
Mor Mordechai Ben <mor6686@gmail.com>
2022-12-20 16:42:26 +0200

    test

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,8 +4,8 @@ import './style.css';
 function Nav() {
   return (
     <div className='nav'>
-      <div className='nav__logo'>App Logo</div>
-      <div className='nav__buttons'>
+      {/* <div className='nav__logo'>App Logo</div>
+      <div className='nav__buttons'> */}
         <Link to='/'>Home</Link>
       </div>
     </div>
